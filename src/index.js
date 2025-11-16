410ce8f48d653665d8926d13f8e4d52c78005a62
Amr Allam <115567652+Amr-Allam@users.noreply.github.com>
2025-11-16 14:07:55 +0200

    Add movie details

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
 import StarRating from "./StarRating";
 
@@ -20,12 +20,12 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={"sss"}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={3} />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
