3cf5838a209fd28b1daddfb3144d52fab7969137
Amr Allam <115567652+Amr-Allam@users.noreply.github.com>
2025-11-23 17:17:30 +0200

    Refactor project into separate files and folders

---
+++ src/components/layout/Box.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Box({ children, className }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className={`box ${className}`}>
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? "–" : "+"}
+      </button>
+
+      {isOpen && children}
+    </div>
+  );
+}
